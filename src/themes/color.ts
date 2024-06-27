c8e63b5235501a82a28ddb85a477e647852eede2
Canh Vo Duc <canh.vo@asnet.com.vn>
2024-06-27 14:30:45 +0700

    Setup theme

---
+++ src/themes/color.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  primary: '#242424',
+  common: {
+    White: '#FFFFFF',
+  },
+  text: {
+    primary: '#303030',
+    secondary: '#606060',
+    tertiary: '#808080',
+    quaternary: '#909090',
+  },
+  border: {
+    primary: '#BDBDBD',
+    secondary: '#E0E0E0',
+    tertiary: '#909090',
+    quaternary: '#F0F0F0',
+  },
+  background: {
+    primary: '#242424',
+    Secondary: '#4F4F4F',
+    tertiary: '#F5F5F5',
+    quaternary: '#B4916C',
+    quinary: '#E4CBAD',
+    senary: '#E0E0E0',
+  },
+};
---
+++ src/themes/typography.ts
@@ -0,0 +1,21 @@
+export const fontSizes = {
+  xs: 12,
+  sm: 14,
+  xmd: 16,
+  md: 18,
+  lg: 20,
+  xl: 24,
+  '2xl': 30,
+  '3xl': 36,
+};
+
+export const lineHeights = {
+  base: 14,
+  xs: 16,
+  sm: 18,
+  md: 20,
+  lg: 24,
+  xl: 28,
+  xxl: 36,
+  '3xl': 38,
+};
