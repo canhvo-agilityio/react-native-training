22e1b65c0f0ee97ed6bb239db0e9bde00328f293
Canh Vo <canh.vo@asnet.com.vn>
2025-03-04 10:23:38 +0700

    Create bottom tab

--- expo-grocery-app/app/(tabs)/_layout.tsx
+++ expo-grocery-app/app/(tabs)/_layout.tsx
@@ -1,9 +1,57 @@
-import { View, Text } from 'react-native';
+import { Tabs } from 'expo-router';
+import {
+  HomeIcon,
+  SearchIcon,
+  StoreIcon,
+  OrderIcon,
+  UserIcon,
+} from '@/components';
+import { colors } from '@/themes';
 
 export default function TabLayout() {
   return (
-    <View>
-      <Text>Tab Layout</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        headerShown: false,
+        tabBarStyle: { height: 86, paddingTop: 13, paddingBottom: 33 },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <HomeIcon color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="browse"
+        options={{
+          title: 'Browse',
+          tabBarIcon: ({ color }) => <SearchIcon color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="my-store"
+        options={{
+          title: 'Store ',
+          tabBarIcon: ({ color }) => <StoreIcon color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="order-history"
+        options={{
+          title: 'Order History',
+          tabBarIcon: ({ color }) => <OrderIcon color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'profile',
+          tabBarIcon: ({ color }) => <UserIcon color={color} />,
+        }}
+      />
+    </Tabs>
   );
 }
---
+++ expo-grocery-app/components/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as HomeIcon } from './Home';
+export { default as SearchIcon } from './Search';
+export { default as StoreIcon } from './Store';
+export { default as OrderIcon } from './Order';
+export { default as UserIcon } from './User';
